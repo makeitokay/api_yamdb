d26433dad7af66f75f3a4ed69dd0d7e33e62b1b3
makeitokay <andrey.mu11@yandex.ru>
2020-04-27 18:44:09 +0500

    модель Review

--- api/models.py
+++ api/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Review(models.Model):
+    title = models.ForeignKey('Title', on_delete=models.CASCADE, related_name='reviews')
+    text = models.TextField(verbose_name='текст отзыва')
+    score = models.PositiveSmallIntegerField(verbose_name='оценка произведения')
